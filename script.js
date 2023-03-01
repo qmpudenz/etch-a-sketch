4af841e4c667f78c8db9fed52c9e0366d3e7e852
Quinn <quinnpudenz@gmail.com>
2023-03-01 13:42:08 -0600

    Prep for buttons, style changes

--- script.js
+++ script.js
@@ -1,14 +1,97 @@
 
-    for (let i=0; i < 256; i++) {
+    for (let i=0; i < 2240; i++) {
         let elementName = document.createElement('div');
-    elementName.style.width = "36px";
-    elementName.style.height = "36px";
-    elementName.style.backgroundColor = "grey";
-    elementName.style.margin = "1px";
-    elementName.addEventListener('mouseover', function(){
-        elementName.style.backgroundColor = "red";
-    });
-    document.body.appendChild(elementName);
-    }
+    elementName.style.width = "1vw";
+    elementName.style.height = "1vw";
+    elementName.style.backgroundColor = "white";
+    elementName.style.margin = "0px";
+    elementName.style.opacity = "0";
 
-    
+    elementName.addEventListener('mouseover', function(){
+        elementName.style.backgroundColor = "black";
+        elementName.style.opacity = ".1";
+        elementName.addEventListener('mouseover', function(){
+            elementName.style.backgroundColor = "black";
+            elementName.style.opacity = ".3";
+            elementName.addEventListener('mouseover', function(){
+                elementName.style.backgroundColor = "black";
+                elementName.style.opacity = ".4";
+                elementName.addEventListener('mouseover', function(){
+                    elementName.style.backgroundColor = "black";
+                    elementName.style.opacity = ".5";
+                    elementName.addEventListener('mouseover', function(){
+                        elementName.style.backgroundColor = "black";
+                        elementName.style.opacity = ".6";
+                        elementName.addEventListener('mouseover', function(){
+                            elementName.style.backgroundColor = "black";
+                            elementName.style.opacity = ".7";
+                            elementName.addEventListener('mouseover', function(){
+                                elementName.style.backgroundColor = "black";
+                                elementName.style.opacity = ".8";
+                                elementName.addEventListener('mouseover', function(){
+                                    elementName.style.backgroundColor = "black";
+                                    elementName.style.opacity = ".9";
+                                    elementName.addEventListener('mouseover', function(){
+                                        elementName.style.backgroundColor = "black";
+                                        elementName.style.opacity = "1";
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
+    document.getElementById('grid').appendChild(elementName);
+};
+
+
+buttonName.addEventListener('click', function(){
+    for (let i=0; i < 450; i++) {
+        let elementName = document.createElement('div');
+    elementName.style.width = "25px";
+    elementName.style.height = "25px";
+    elementName.style.backgroundColor = "white";
+    elementName.style.margin = "0px";
+    elementName.style.opacity = "0";
+
+    elementName.addEventListener('mouseover', function(){
+        elementName.style.backgroundColor = "black";
+        elementName.style.opacity = ".1";
+        elementName.addEventListener('mouseover', function(){
+            elementName.style.backgroundColor = "black";
+            elementName.style.opacity = ".3";
+            elementName.addEventListener('mouseover', function(){
+                elementName.style.backgroundColor = "black";
+                elementName.style.opacity = ".4";
+                elementName.addEventListener('mouseover', function(){
+                    elementName.style.backgroundColor = "black";
+                    elementName.style.opacity = ".5";
+                    elementName.addEventListener('mouseover', function(){
+                        elementName.style.backgroundColor = "black";
+                        elementName.style.opacity = ".6";
+                        elementName.addEventListener('mouseover', function(){
+                            elementName.style.backgroundColor = "black";
+                            elementName.style.opacity = ".7";
+                            elementName.addEventListener('mouseover', function(){
+                                elementName.style.backgroundColor = "black";
+                                elementName.style.opacity = ".8";
+                                elementName.addEventListener('mouseover', function(){
+                                    elementName.style.backgroundColor = "black";
+                                    elementName.style.opacity = ".9";
+                                    elementName.addEventListener('mouseover', function(){
+                                        elementName.style.backgroundColor = "black";
+                                        elementName.style.opacity = "1";
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
+    document.getElementById('grid').appendChild(elementName);
+};
+});
