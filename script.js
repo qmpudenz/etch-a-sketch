63c87d789df55c866d9547d7f9354e654890544e
Quinn <quinnpudenz@gmail.com>
2023-08-10 23:18:16 -0500

    Large, med, small and custom grid added.

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ function createGrid(size) {
   let grid = document.getElementById("grid");
   grid.innerHTML = ""; // Clear existing grid
 
+  // Adjust the grid layout according to the size
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
   for (let i = 0; i < size * size; i++) {
     let cell = document.createElement("div");
     cell.style.opacity = "0";
@@ -16,5 +20,28 @@ function createGrid(size) {
   }
 }
 
-// Initialize the grid with 16x16 size
+document.getElementById("createNewGrid").addEventListener("click", function () {
+  let size = prompt("Enter the size of the grid (e.g., 16 for a 16x16 grid):");
+  size = parseInt(size);
+  if (!isNaN(size) && size > 0) {
+    createGrid(size);
+  } else {
+    alert("Invalid size. Please enter a positive integer.");
+  }
+});
+
+// Buttons logic
+document.getElementById("large").addEventListener("click", function () {
+  createGrid(16);
+});
+
+document.getElementById("medium").addEventListener("click", function () {
+  createGrid(32);
+});
+
+document.getElementById("small").addEventListener("click", function () {
+  createGrid(64);
+});
+
+// Initialize the grid with 16x16 size by default
 createGrid(16);
